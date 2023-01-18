d0c62d5abc89bc523e49b78811c27017786ed3c9
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-01-18 15:59:36 +1300

    re-construct

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+
+  const onClick = () => {
+    console.log(input);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
