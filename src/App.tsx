73f1e5482c78afd10e47f84f1ea8a9b5d30d1a8f
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-01-19 16:13:35 +1300

    pre mod esbuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
+import { unpkgPathPlugin } from "./plugins/upkg";
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
+ const startService = async () => {
+  ref.current = await esbuild.startService({
+    worker: true,
+    wasmURL: "/esbuild.wasm",
+  })
+ }
 
-  const onClick = () => {
-    console.log(input);
+ useEffect(() => {
+    startService()
+ }, [])
+
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    };
+    const result = await ref.current.build({
+     entryPoints: ['index.js'],
+     bundle: true,
+     write: false,
+     plugins: [unpkgPathPlugin()]
+    })
+    console.log(result)
+    setCode(result.outputFiles[0].text)
   }
 
   return (
