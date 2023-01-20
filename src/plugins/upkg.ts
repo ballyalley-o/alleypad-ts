7fab31fb1508201e7a6fcbda1fee590bad7721f6
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-01-20 15:04:02 +1300

    unpkg URL gen

--- src/plugins/upkg.ts
+++ src/plugins/upkg.ts
@@ -1,12 +1,26 @@
 import * as esbuild from "esbuild-wasm";
+import axios from 'axios'
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
-        return { path: args.path, namespace: "a" };
+        console.log("onResolve", args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: "a" }
+        }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: "a",
+            path: new URL(args.path, 'https://unpkg.com'+ args.resolveDir + '/').href
+          }
+        }
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,16 +30,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
-              console.log(message);
+              import React, { useState } from 'react'
+              console.log(React, useState);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
         }
+
+        const { data, request } = await axios.get(args.path);
+        console.log(request)
+         return {
+            loader: "jsx",
+            contents: data,
+            resolveDir: new URL ('./', request.responseURL).pathname
+          };
       });
     },
   };
