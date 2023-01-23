8b29207e230846b6b1239a2af036a340437570d3
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-01-23 15:42:51 +1300

    fetch link esbuild wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import { unpkgPathPlugin } from "./plugins/upkg";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
@@ -10,7 +11,7 @@ const App = () => {
  const startService = async () => {
   ref.current = await esbuild.startService({
     worker: true,
-    wasmURL: "/esbuild.wasm",
+    wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
   })
  }
 
@@ -27,7 +28,10 @@ const App = () => {
      entryPoints: ['index.js'],
      bundle: true,
      write: false,
-     plugins: [unpkgPathPlugin()],
+     plugins: [
+          unpkgPathPlugin(),
+          fetchPlugin(input)
+      ],
      define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window'
@@ -53,5 +57,6 @@ const App = () => {
     </>
   );
 };
+
 
 export default App;
